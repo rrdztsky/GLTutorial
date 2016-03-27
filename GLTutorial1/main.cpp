a459697691bede27e1c93235e2cbdfcada13cb74
Rachel Radeztsky <rachel@rachels-macbook-air.local>
2016-03-27 15:23:57 -0700

    Initial Commit

---
+++ GLTutorial1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GLTutorial1
+//
+//  Created by Rachel Radeztsky on 3/27/16.
+//  Copyright © 2016 Rachel Radeztsky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
